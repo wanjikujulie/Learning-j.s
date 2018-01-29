678a43676dfc04aa149c979544f99a0a7129c3fa
Student <julienjiku@gmail.com>
2018-01-29 14:18:09 +0300

    first commit

---
+++ julie_cpp.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int addition(int x,int y){int z=x+y;
+return z;
+}    
+}
+int main()
+{
+   int a;
+   int b;
+   cout<<"enter first number";
+   cin>>a;
+   cout<<"enter second number";
+   cin>>b;
+   cout<<a;cout<<"+";cout<<b;cout<<"=";cout<<addition(a,b);
+
+    return 0;
+}
+
+
+
