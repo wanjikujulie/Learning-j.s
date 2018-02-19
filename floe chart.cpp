796e3678be8a17ae0a34a944a984223e86ad4672
Student <julienjiku@gmail.com>
2018-02-19 16:35:41 +0300

    fifth commit

---
+++ floe chart.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+ using namespace std;
+ int main()
+ {
+ int a;
+ int b;
+ 
+  cout<<"first number";
+    cin>> a;
+    cout<<"second nuber";
+    cin>>b;
+if(a>b)
+ cout<<"a>b";
+ else if (a==b);
+ cout<<"a==b";
+ 
+ return 0;
+ 
+ }
+
